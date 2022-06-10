2a0c3af3393557ed4321c4a52a2394e6a7c7356f
DESKTOP-BP73NMV\PC-25 <cacao621@naver.com>
2022-06-10 12:11:26 +0900

    memberRestController/fileupload, memberController

--- src/main/java/kr/or/ddit/controller/MemberContoller.java
+++ src/main/java/kr/or/ddit/controller/MemberContoller.java
@@ -4,19 +4,27 @@ import java.sql.SQLException;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.jsp.command.MemberRegistCommand;
 import com.jsp.command.SearchCriteria;
+import com.jsp.controller.MakeFileName;
+import com.jsp.dto.MemberVO;
 import com.jsp.service.LoginSearchMemberService;
+
+import kr.or.ddit.command.MemberModifyCommand;
 
 @Controller
 @RequestMapping(value="/member")
-public class MemberContoller {
+public class MemberContoller{
 	
 	@Autowired
 	private LoginSearchMemberService memberService;
@@ -49,4 +57,74 @@ public class MemberContoller {
 		String url = "member/regist";
 		return url;
 	}
+	
+	@RequestMapping(value="/regist", method=RequestMethod.POST)
+	public String regist(MemberRegistCommand memberReq) throws Exception{
+		String url = "member/regist_success";
+		
+		MemberVO member = memberReq.toMemberVO();
+		memberService.regist(member);
+		
+		return url;
+	}
+	
+	@RequestMapping(value="/detail", method=RequestMethod.GET)
+	public String detail(String id, Model model)throws Exception{
+		String url = "member/detail";
+		
+		MemberVO member = memberService.getMember(id);
+		
+		model.addAttribute("member",member);
+		
+		return url;
+	}
+	
+	
+	@RequestMapping(value="/modifyForm", method=RequestMethod.GET)
+	public String modifyForm(String id, Model model) throws Exception{
+		String url = "member/modify";
+		
+		MemberVO member = memberService.getMember(id);
+		
+		String picture = MakeFileName.parseFileNameFromUUID(member.getPicture(), "\\$\\$");
+		member.setPicture(picture);
+	
+		model.addAttribute("member", member);
+		
+		return url;
+	}
+	
+	@Autowired
+	MemberRestController restController;
+	
+	@RequestMapping(value="/modify", method=RequestMethod.POST, produces = "text/plain;charset=utf-8")
+	public String modify(MemberModifyCommand modifyReq, HttpSession session, RedirectAttributes rttr) throws Exception{
+		String url = "redirect:/member/detail.do";
+		
+		MemberVO member = modifyReq.toMember();
+		
+		// 신규 파일 변경 및 기존 파일 삭제
+		String oldPicture = memberService.getMember(member.getId()).getPicture();
+		if(modifyReq.getUploadPicture() != null && !modifyReq.getUploadPicture().isEmpty()) {
+			String fileName = restController.savePicture(oldPicture, modifyReq.getPicture());
+			member.setPicture(fileName);
+		}else {
+			member.setPicture(oldPicture);
+		}
+		
+		memberService.modify(member);
+		
+		return url;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/kr/or/ddit/controller/MemberRestController.java
+++ src/main/java/kr/or/ddit/controller/MemberRestController.java
@@ -1,7 +1,12 @@
 package kr.or.ddit.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
 import javax.annotation.Resource;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.jsp.controller.MakeFileName;
+import com.jsp.dto.MemberVO;
 import com.jsp.service.LoginSearchMemberService;
 
 @RestController
@@ -23,6 +30,34 @@ public class MemberRestController {
 	@Resource(name="picturePath")
 	private String picturePath;
 	
+	public String savePicture(String oldPicture, MultipartFile multi) throws Exception{
+		String fileName = null;
+		
+		/* 저장할 폴더 설정 */
+		String uploadPath = picturePath;
+		
+		if(!(multi == null || multi.isEmpty() || multi.getSize() > 1024 * 1024 * 1)) {
+			fileName = MakeFileName.toUUIDFileName(multi.getOriginalFilename(), "$$");
+			File storeFile = new File(uploadPath, fileName);
+			
+			//파일경로 생성
+			storeFile.mkdirs();
+			
+			//저장
+			multi.transferTo(storeFile);
+		}
+		
+		//기존파일삭제
+		if(oldPicture != null && !oldPicture.isEmpty()) {
+			File oldFile = new File(uploadPath, oldPicture);
+			if(oldFile.exists()) {
+				oldFile.delete();
+			}
+		}
+		
+		return fileName;
+	}
+	
 	@RequestMapping(value="/picture", method=RequestMethod.POST, produces="text/plain;charset=utf-8")
 	public ResponseEntity<String> picture(@RequestParam("pictureFile") MultipartFile multi, String oldPicture)
 											throws Exception{
@@ -30,8 +65,49 @@ public class MemberRestController {
 		String result = "";
 		HttpStatus status = null;
 		
+		/* 파일저장확인 */
+		if((result = savePicture(oldPicture, multi)) == null) {
+			result = "업로드를 실패했습니다.!";
+			status = HttpStatus.BAD_REQUEST;
+		}else {
+			status = HttpStatus.OK;
+		}
 		
 		entity = new ResponseEntity<String>(result, status);
 		return entity;
 	}
+	
+	@RequestMapping(value="/getPicture", produces = "text/plain;charset=utf-8")
+	public ResponseEntity<byte[]> getPicture(String id) throws Exception{
+		String picture = memberService.getMember(id).getPicture();
+		
+		InputStream in = null;
+		ResponseEntity<byte[]> entity = null;
+		String imgPath = picturePath;
+		
+		try {
+			in = new FileInputStream(new File(imgPath, picture));
+			
+			entity = new ResponseEntity<byte[]>(IOUtils.toByteArray(in), HttpStatus.CREATED); 
+		}finally {
+			in.close();
+		}
+		
+		return entity;
+	}
+	
+	@RequestMapping("/idCheck")
+	public ResponseEntity<String> idCheck(String id) throws Exception{
+		ResponseEntity<String> entity = null;
+		
+		MemberVO member = memberService.getMember(id);
+		if(member != null) {
+			entity = new ResponseEntity<String>("duplicated", HttpStatus.OK);
+		}else {			
+			entity = new ResponseEntity<String>("", HttpStatus.OK);
+		}
+		
+		return entity;
+	}
+	
 }
